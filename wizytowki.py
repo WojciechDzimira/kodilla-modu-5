4a379df92468401415bdc4969a1a1903897b1b87
Wojciech Dzimira <wojciech.dzimira@gmail.com>
2026-03-09 10:18:08 +0100

    Refactor contact creation functions by removing unused kwargs and improving logging messages

--- wizytowki.py
+++ wizytowki.py
@@ -5,7 +5,7 @@ fake = Faker('pl_PL')
 
 class BaseContact:
     """ klasa reprezentująca prywatną wizytówkę """
-    def __init__(self, owner_name, owner_surname, owner_e_mail, owner_private_number, **kwargs):
+    def __init__(self, owner_name, owner_surname, owner_e_mail, owner_private_number): #wykasowany **kwargs
         self.name = owner_name
         self.surname = owner_surname
         self.e_mail = owner_e_mail
@@ -23,13 +23,13 @@ class BaseContact:
     
 class BusinessContact(BaseContact):
     def __init__(self, owner_name, owner_surname, owner_e_mail, owner_private_number, 
-                 owner_firm_name, owner_position, owner_business_number, **kwargs):
+                 owner_firm_name, owner_position, owner_business_number):   #wykasowany **kwargs
         super().__init__(
             owner_name=owner_name, 
             owner_surname=owner_surname, 
             owner_e_mail=owner_e_mail, 
-            owner_private_number=owner_private_number, 
-            **kwargs)
+            owner_private_number=owner_private_number 
+            )
         self.position = owner_position
         self.firm_name = owner_firm_name
         self.business_number = owner_business_number
@@ -48,10 +48,10 @@ def int_input(text):
                 return number
             else:
                 print("to nie jest dodatnia liczba całkowita, wprowadz jeszcze raz.")
-                logging.warning(f"Użytkownik wprowadził liczbę ujemną")
+                logging.warning("Użytkownik wprowadził liczbę ujemną")
         except ValueError:
             print("to nie jest liczba całkowita, wprowadz jeszcze raz.")
-            logging.warning(f"Użytkownik wprowadził wartośc nie będącą liczbą")
+            logging.warning("Użytkownik wprowadził wartośc nie będącą liczbą")
 
 def card_choosing():
     """"Funkcja wyboru rodzaju wizytówek jakie mają byc wygenerowane"""
@@ -63,18 +63,13 @@ def card_choosing():
             print("Błąd, wpisz jeszcze raz")  
             logging.warning(f"Użytkownik wprowadził niepoprawną wartość")           
 
-def create_contacts():
+def create_contacts(choice_card, choice_card_number):
     """Funkcja pobiera od uzytkowniak jaki rodzaj wizytowek i jaka ich ilosc chce wygenerowac 
     po czym tworzy je i zapisuje na odpowieniej liście"""
     
     business_contact_list = []
     base_contact_list = []
 
-    print("1: stwórz prywatną wizytówkę")
-    print("2: stwórz biznesową wizytówkę")
-    choice_card = card_choosing()
-    
-    choice_card_number = int_input("Wpisz ile wizytówek chcesz stworzyć: ")
     if choice_card == 1:
         for i in range(choice_card_number):
             card = BaseContact(
@@ -86,7 +81,7 @@ def create_contacts():
             base_contact_list.append(card)
             logging.debug(f"Stworzono {i+1}/{choice_card_number} wizytówek prywatnych")
         return base_contact_list
-    elif choice_card == 2:
+    elif choice_card == 1:
         for i in range(choice_card_number):
             card = BusinessContact(
             owner_name = fake.first_name(),
@@ -103,5 +98,10 @@ def create_contacts():
 
 if __name__ == "__main__":        
     logging.basicConfig(level=logging.DEBUG)
-    list = create_contacts()
     
+
+    print("1: stwórz prywatną wizytówkę")
+    print("2: stwórz biznesową wizytówkę")
+    choice_card = card_choosing()
+    list = create_contacts(choice_card, int_input("Ile wizytówek stworzyć? "))
+   
